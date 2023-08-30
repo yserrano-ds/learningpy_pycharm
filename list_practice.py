7610e1158f754b8d4a9db4df69703af4b25e43c7
Yonny Serrano <yonny.serrano@gmail.com>
2023-08-29 19:14:32 -0600

    Análisis de patrones de strings con regexp

--- list_practice.py
+++ list_practice.py
@@ -1,5 +1,21 @@
-a = [1, 2, 3, 4, 5]
-b = [2, 4, 6, 8]
+# a = [1, 2, 3, 4, 5]
+# b = [2, 4, 6, 8]
+# print(b.count(1))
+# print(len(b))
 
-print(b.count(1))
-print(len(b))
+import re
+fh = open('elaleph_cuentos.txt')
+libro = []
+for line in fh:
+    # nombres = re.findall('[A-Z][a-z]+', line)
+    nombres = re.findall('[a-z] [A-Z][a-z]+', line)
+    if nombres != []:
+        i = 0
+        for i in range(len(nombres)):
+            x, n = nombres[i].split(' ')
+            if libro.count(n) == 0:
+                libro.append(n)
+        # libro += nombres
+print(f'Patrones encontrados: {len(libro)}\nPrimeros 20:\n')
+for i in range(20):
+    print(libro[i])
--- main.py
+++ main.py
@@ -6,11 +6,12 @@
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+    x, n = name.split(' ')
+    print(f'Hi, {x}#')  # Press Ctrl+F8 to toggle the breakpoint.
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    print_hi('a PyCharm')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
